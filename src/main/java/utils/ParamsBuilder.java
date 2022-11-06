12aa615dad6c5e3d39615379d15313534b1563ef
Abdel Wadoud <88406384+rasmi-aw@users.noreply.github.com>
2022-11-06 23:01:14 +0100

    Update params builder

--- src/main/java/utils/ParamsBuilder.java
+++ src/main/java/utils/ParamsBuilder.java
@@ -2,6 +2,10 @@ package utils;
 
 import lombok.Getter;
 import lombok.Setter;
+import params.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Abdelwadoud Rasmi
@@ -10,7 +14,57 @@ import lombok.Setter;
 public class ParamsBuilder {
 
     @Getter
-    @Setter
-    private String ;
+    private Set<Param> params;
 
+    private ParamsBuilder() {
+        params = new HashSet<>();
+    }
+
+    /**
+     * Get builder instance
+     */
+    public static final ParamsBuilder get() {
+        return new ParamsBuilder();
+    }
+
+    /**
+     * Add param to the list of params
+     */
+    public final ParamsBuilder add(Param param) {
+        if (param != null && param.isValid()) {
+            params.remove(param);
+            params.add(param);
+        }
+        return this;
+    }
+
+    public final ParamsBuilder setAcceptUrl(AcceptURL acceptURL) {
+        add(acceptURL);
+        return this;
+    }
+
+    public final ParamsBuilder setCallbackUrl(CallbackURL callbackUrl) {
+        add(callbackUrl);
+        return this;
+    }
+
+    public final ParamsBuilder setCancelUrl(CancelURL cancelUrl) {
+        add(cancelUrl);
+        return this;
+    }
+
+    public final ParamsBuilder setOrderId(OrderId orderId) {
+        add(orderId);
+        return this;
+    }
+
+    public final ParamsBuilder setProjectId(ProjectId projectId) {
+        add(projectId);
+        return this;
+    }
+
+    public final ParamsBuilder setVersion(Version version) {
+        add(version);
+        return this;
+    }
 }
